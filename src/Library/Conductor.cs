66db7ee181410cb310a7e19bc44e8d2cfa24156b
facumar93 <53872293+facumar93@users.noreply.github.com>
2020-05-07 22:23:49 -0300

    Agrego Clase Conductor

---
+++ src/Library/Conductor.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PII_Herencia
+{
+public class Conductor : Usuario
+    {
+        public Conductor(string name, string lastname, string ci) : base(name, lastname, ci)
+        { }
+
+        
+    }
+}
